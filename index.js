52a9b4bbce28d466cb84a1e532195c29f8ff3438
sunga-lee <sungajunior1@gmail.com>
2023-03-24 07:55:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+describe('index.js', function () {
+    describe('scuberGreetingForFeet()', function () {
+      it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
+        expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+      });
+  
+      // tests continue...
+    });
+  });
